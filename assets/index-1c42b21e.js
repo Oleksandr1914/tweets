f66b6dbcb3b8602b6e1c152f7510fc128dbe793d
Oleksandr1914 <Oleksandr1914@users.noreply.github.com>
2023-06-15 19:00:03 +0000

    deploy: afceca72aeb47d6cc498e638f1aeb773e56784ab

--- assets/index-1c42b21e.js
+++ assets/index-1c42b21e.js
@@ -288,7 +288,7 @@ select:-webkit-autofill:focus {
   margin: 0 auto;
   margin-top: 80px;
   padding: 14px;
-  padding-right: 20px;
+  padding-right: 30px;
   width: 196px;
   background-color: #471ca9;
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
